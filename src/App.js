f627038869f1c6ab9f3c6ff6c82f88d1365954c5
Ankit-cg22 <ankitdas.cg22@gmail.com>
2021-08-06 18:01:11 +0530

    bg-image and scroll-to-top

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
   
   const [loading , setLoading] = useState(false);
   const [load , setLoad] = useState(false);
+  const [op , setOp] = useState(0);
   
   useEffect(()=>{
     setLoading(true)
@@ -29,20 +30,19 @@ function App() {
   const [dishes , setDishes ] = useState([])
   
   const [queryTemp , setQueryTemp]= useState("") // temporary query
-  const [query , setQuery]= useState("chicken")
+  const [query , setQuery]= useState("")
 
 
   const updateQueryTemp = e => {
     setQueryTemp(e.target.value);
-    console.log(queryTemp);
+
   }
 
   const updateFinalQuery = e => {
     // runs when we submit the form
     e.preventDefault();
-    console.log(queryTemp)
     setQuery(queryTemp)
-    console.log(query)
+
     setQueryTemp("")
     setLoad(!load)
   }
@@ -58,21 +58,43 @@ function App() {
     setDishes(data.hits)
   }
 
+
+  //=-=-=-=-=-= Scroll to top =-=-=-=-=-=-
+  function scrollUp() {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth" // for smooth scroll up
+    })
+  }
+
+  window.addEventListener('scroll', () => {
+       if(window.scrollY > 500)setOp(1);
+       else setOp(0);
+  })
+
+
+
   return (
     <div className="App">
-      <h1>Recipe App</h1>
-        <form onSubmit={updateFinalQuery}>
-        
-            <input className="searchBar" type="text" value={queryTemp} onChange={updateQueryTemp} placeholder =" Seach your dish..."/>    
-            <button className="button" type="submit"> Search </button>
+        <div class="header">
+          <div class="head">
+            <div class="name">
+              <h1>Recipe App</h1>
+            </div>
+          </div>
+          <form onSubmit={updateFinalQuery}>
           
-        </form>
+              <input className="searchBar" type="text" value={queryTemp} onChange={updateQueryTemp} placeholder =" Seach your dish..."/>
+              <button className="button" type="submit"> Search </button>
+          
+          </form>
+        </div>
 
         {
           loading ?
             <div class="preloader">
               <h1>Fetching Dishes....</h1>
-              <HashLoader color='black' loading={loading}  size={50} />
+              <HashLoader color='white' loading={loading}  size={50} />
             </div>
           :
             <div className="dishesContainer">
@@ -87,6 +109,13 @@ function App() {
             })}
           </div>
         }
+
+       <div className="scroll-top" onClick={scrollUp} style = {{opacity: op}} >
+          <svg id="arrow" viewBox="0 0 24 24">
+              <path fill="currentColor" d="M13,20H11V8L5.5,13.5L4.08,12.08L12,4.16L19.92,12.08L18.5,13.5L13,8V20Z" />
+          </svg>  
+       </div>
+
     </div>
   );
 }
